a4ec50c7d4df1d1cb5d9e5571752288202470c4d
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-07 17:01:37 -0300

    instâncias das três classes singletons

--- design-patterns/src/creational/singleton/module_a.ts
+++ design-patterns/src/creational/singleton/module_a.ts
@@ -1,13 +1,9 @@
+import { MyDatabaseFunction } from './db/my-database-function';
+import { MyDatabaseModule } from './db/my-database-module';
 import { MyDatabaseClassic } from './db/my-database-classic';
 
-const db1 = MyDatabaseClassic.getInstance();
-const db2 = MyDatabaseClassic.getInstance();
-console.log(db1 === db2);
+const db1 = MyDatabaseModule;
+const db2 = MyDatabaseFunction;
+const db3 = MyDatabaseClassic.instance;
 
-db1.addUser({name: 'John', age: 30});
-db1.addUser({name: 'Jane', age: 25});
-
-db1.removeUser(0);
-db1.showUsers();
-
-export { db1, db2 };
+export { db1, db2, db3 };
