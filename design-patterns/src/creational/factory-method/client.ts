32e583d0ba93eadd39e46f640f33507693561fd8
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-19 21:08:05 -0300

    criação de vários objetos em laço via FactoryMethod

--- design-patterns/src/creational/factory-method/client.ts
+++ design-patterns/src/creational/factory-method/client.ts
@@ -1,5 +1,7 @@
 import { BicycleFactory } from "./factories/bicycle-factory";
 import { CarFactory } from "./factories/car-factory";
+import { randomVehicleAlgorithm } from "./main/random-vehicle-algorithm";
+import { randomNumberLength } from "./utils/random-numbers-length";
 
 const carFactory = new CarFactory();
 const bicycleFactory = new BicycleFactory();
@@ -8,3 +10,14 @@ const fusca = carFactory.getVehicle('Fusca');
 
 fusca.pickUp('Luiz');
 fusca.stop();
+
+const customerNames = ['Luiz', 'Felipe'];
+console.log(customerNames.length);
+
+for (let i = 0; i < 10; i++) {
+    const vehicle = randomVehicleAlgorithm();
+    const name = customerNames[randomNumberLength(customerNames.length-1)];
+
+    vehicle.pickUp(name);
+    vehicle.stop();
+}
