995fecad08776a4858219fa0a78af68c53b77f69
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-30 16:07:39 -0300

    exemplo de utilização com classe e função

--- design-patterns/src/structural/adapter/client.ts
+++ design-patterns/src/structural/adapter/client.ts
@@ -1,9 +1,17 @@
-import isEmail from 'validator/lib/isEmail';
+import { EmailValidatorClassAdapter } from "./validation/email-validator-class-adapter";
+import { emailValidatorFunctionAdapter } from "./validation/email-validator-func-adapter";
 
 const email = 'email@email.com';
 
-if (isEmail(email)) {
-    console.log('É válido');
+const emailValidator = new EmailValidatorClassAdapter();
+if (emailValidator.isEmail(email)) {
+    console.log('É válido - classe');
 } else {
-    console.log('É inválido');
+    console.log('É inválido - classe');
+}
+
+if (emailValidatorFunctionAdapter(email)) {
+    console.log('É válido - função');
+} else {
+    console.log('É inválido - função');
 }
