745fda91dcd442280771b9108a0b69f63d0efcea
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-02 22:12:08 -0300

    interface de implementação dos dispositivos

---
+++ design-patterns/src/structural/bridge/device/device-implementation.ts
@@ -0,0 +1,7 @@
+export interface DeviceImplamentation {
+    getName(): string;
+    setPower(powerStatus: boolean): void;
+    getPower(): boolean;
+    setVolume(volume: number): void;
+    getVolume(): number;
+}
