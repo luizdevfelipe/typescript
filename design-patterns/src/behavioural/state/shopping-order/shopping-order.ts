23dc7f40d8b8625c600605ba1f3c8f9705d694af
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-29 15:34:37 -0300

    classe Context delegando suas ações para os estados

--- design-patterns/src/behavioural/state/shopping-order/shopping-order.ts
+++ design-patterns/src/behavioural/state/shopping-order/shopping-order.ts
@@ -1,4 +1,37 @@
+import { OrderPending } from "./order-pending";
+import { ShoppingOrderState } from "./shopping-order-state-interfacel";
+
 // Class that depends on multiple states
 export class ShoppingOrder {
     private state: ShoppingOrderState = new OrderPending(this);
+
+    getState(): ShoppingOrderState {
+        return this.state;
+    }
+
+    setState(state: ShoppingOrderState): void {
+        this.state = state;
+        console.log(`Order state changed to: ${this.getStateName()}`);
+    }
+
+    // Emphasizes that the ContextClass can have different methods than the State classes
+    getStateName(): string {
+        return this.state.getName();
+    }
+
+    approvePayment(): void {
+        this.state.approvePayment();
+    }
+
+    rejectPayment(): void {
+        this.state.rejectPayment();
+    }
+
+    waitPayment(): void {
+        this.state.waitPayment();
+    }
+
+    shipOrder(): void {
+        this.state.shipOrder();
+    }
 }
