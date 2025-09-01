3d43a1ceef5ef8341289f713b96644c07fa47c8c
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-09-01 16:32:26 -0300

    teste com diferentes estados para um pedido

--- design-patterns/src/behavioural/state/main.ts
+++ design-patterns/src/behavioural/state/main.ts
@@ -1,1 +1,13 @@
 // npx ts-node --files --transpile-only src/behavioural/state/main.ts
+
+import { ShoppingOrder } from "./shopping-order/shopping-order";
+
+const order = new ShoppingOrder(); // Pending
+order.approvePayment(); // Approved
+order.waitPayment(); // Pending
+order.shipOrder(); // Shipped
+
+order.rejectPayment(); // Rejected
+order.approvePayment(); // Fail
+order.shipOrder(); // Fail
+
