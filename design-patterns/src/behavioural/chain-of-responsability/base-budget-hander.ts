6029bcf011609ecb4a48ceb43d4ef27db901c20b
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-09-09 15:35:16 -0300

    Handler que será utilizado como base

---
+++ design-patterns/src/behavioural/chain-of-responsability/base-budget-hander.ts
@@ -0,0 +1,16 @@
+import { CustomerBudget } from "./customer-budget";
+
+// Base Handler class for the Chain of Responsibility pattern
+export abstract class BaseBudgetHandler {
+    protected nextHandler: BaseBudgetHandler | null = null;
+
+    setNext(handler: BaseBudgetHandler): BaseBudgetHandler {
+        this.nextHandler = handler;
+        return handler;
+    }
+    
+    handle(budget: CustomerBudget): CustomerBudget {
+        if (this.nextHandler) return this.nextHandler.handle(budget);
+        return budget;
+    }
+}
