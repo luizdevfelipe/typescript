2c4ab32452c70cb83740c6e0ee2287a7c05cbf93
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-09-29 16:07:44 -0300

    Observers e código cliente

--- design-patterns/src/behavioural/observer/observer.ts
+++ design-patterns/src/behavioural/observer/observer.ts
@@ -10,10 +10,11 @@ interface Observer {
     update(...args: unknown[]): void;
 }
 
+// Concrete Observable
 class InputObservable implements Observable {
     private observers: Observer[] = [];
 
-    constructor(public input: HTMLInputElement) { }
+    constructor(public element: HTMLInputElement) { }
 
     subscribe(...observers: Observer[]): void {
         observers.forEach(observer => {
@@ -32,3 +33,56 @@ class InputObservable implements Observable {
         this.observers.forEach(observer => observer.update(this));
     }
 }
+
+// Concrete Observer
+class ParagraphObserver implements Observer {
+    constructor(public element: HTMLParagraphElement) {}
+
+    update(observable: Observable): void {
+        if (observable instanceof InputObservable) {
+            this.element.innerText = observable.element.value;
+        }
+    }
+}
+
+class DivObserver implements Observer {
+    constructor(public element: HTMLDivElement) {}
+
+    update(observable: Observable): void {
+        if (observable instanceof InputObservable) {
+            this.element.innerText = observable.element.value;
+        }
+    }
+}
+
+// CLIENTE CODE
+function makeInput(): HTMLInputElement {
+    const input = document.createElement('input');
+    document.body.appendChild(input);
+    return input;
+}
+
+function makeParagraph(): HTMLParagraphElement {
+    const p = document.createElement('p');
+    document.body.appendChild(p);
+    p.innerText = 'Texto inicial';
+    return p;
+}
+
+function makeDiv(): HTMLDivElement {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    div.innerText = 'Texto em div';
+    return div;
+}
+
+const input = new InputObservable(makeInput());
+const p1 = new ParagraphObserver(makeParagraph());
+const p2 = new ParagraphObserver(makeParagraph());
+const div = new DivObserver(makeDiv());
+
+input.subscribe(p1, p2, div);
+
+input.element.addEventListener('input', function () {
+    input.notify();
+})
