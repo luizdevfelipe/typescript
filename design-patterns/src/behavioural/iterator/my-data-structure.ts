a822b8398a9338bc881dbc040c0315b3df2afb5f
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-10-01 17:11:25 -0300

    Create my-data-structure.ts

---
+++ design-patterns/src/behavioural/iterator/my-data-structure.ts
@@ -0,0 +1,19 @@
+import { MyDefaultIterator } from "./my-default-iterator";
+import { MyIteratorProtocol } from "./my-iterator-protocol";
+
+export class MyDataStructure {
+    private _items: string[] = [];
+    private iterator: MyIteratorProtocol<string> = new MyDefaultIterator(this);
+
+    addItem(...items: string[]): void {
+        items.forEach(item => this.items.push(item));
+    }
+
+    get items(): string[] {
+        return this._items;
+    }
+
+    size(): number {
+        return this.items.length;
+    }
+}
