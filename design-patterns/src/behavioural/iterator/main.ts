fc18d2e473219bf71fa3d4e19c3c22bec399ceff
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-10-03 10:39:19 -0300

    Trocando Iterator e utilizando lazy evaluation
    - Novo Iterator para percorrer a coleção de trás para frente, alterado em tempo de execução
    - Método resetIterator() permite avançar com a iteração apenas quando necessário

--- design-patterns/src/behavioural/iterator/main.ts
+++ design-patterns/src/behavioural/iterator/main.ts
@@ -1,14 +1,27 @@
 // npx ts-node --files --transpile-only src/behavioural/iterator/main.ts
 
 import { MyDataStructure } from "./my-data-structure";
+import { MyReverseIterator } from "./my-reverse-iterator";
 
 const dataStructure = new MyDataStructure();
 dataStructure.addItem('A', 'B', 'C', 'D', 'E', 'F');
 
 const [a, b] = dataStructure;
-
 console.log(a, b);
-console.log();
+
+const [c, ...rest] = dataStructure;
+console.log(c, rest);
+
+console.log('...\n');
+
+dataStructure.resetIterator();
+for (const data of dataStructure) {
+    console.log(data);
+}
+
+console.log('...\n');
+
+dataStructure.changeIterator(new MyReverseIterator(dataStructure));
 
 for (const data of dataStructure) {
     console.log(data);
